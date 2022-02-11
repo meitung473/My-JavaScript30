bdc2cc8442a49a20f3a589b48fbfeb48269e7f0b
meitung473 <a8870506@gmail.com>
2022-02-11 11:47:31 +0800

    init

---
+++ 01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,23 @@
+function playSound(e) {
+    console.log(e);
+    let key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add("playing");
+    //
+    audio.addEventListener("ended", function () {
+        key.classList.remove("playing");
+    });
+}
+
+window.addEventListener("keydown", playSound);
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) =>
+    key.addEventListener("transitionend", function (e) {
+        if (e.propertyName !== "transform") return;
+        e.target.classList.remove("playing");
+    })
+);
---
+++ readme.md
@@ -0,0 +1,15 @@
+## Javascript 30 
+
+
+|序 |筆記 | 程式碼|
+| --- | ---| ---|
+| 01 - JavaScript Drum Kit | [筆記](/01 - JavaScript Drum Kit/memo_01.md)|[source](./01 - JavaScript Drum Kit/script.js)|
+| 02 - JS and CSS Clock| | |
+| 03 - CSS Variables| | |
+| 04 - Array Cardio Day 1| | |
+| 05 - Flex Panel Gallery| | |
+| 06 - Type Ahead| | |
+| 07 - Array Cardio Day 2| | |
+| 08 - Fun with HTML5 Canvas| | |
+| 09 - Dev Tools Domination| | |
+| 10 - Hold Shift and Check Checkboxes| | |
